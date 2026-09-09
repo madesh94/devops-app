5573b09f01df95f798219fda30a96c837863c052
madesh <madeshak94@gmail.com>
2026-09-09 12:36:02 +0530

    initial commit - DevOps Application

---
+++ app.py
@@ -0,0 +1,13 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+class MyHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b"Hello from my DevOps application!")
+
+server = HTTPServer(("0.0.0.0", 8080), MyHandler)
+
+print("Application running on port 8080...")
+
+server.serve_forever()
